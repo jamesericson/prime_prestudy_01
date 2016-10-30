e25ef23f618b39cd9a8d182530cc04fb042d6bc0
James Ericson <jamesericson@gmail.com>
2016-10-30 16:08:58 -0500

    added a click function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Is it working?");
+console.log("Hey this works!");
+var clickNo = 0;
+console.log("clickNo = " + clickNo);
+
+function clicked () {
+
+  if (clickNo%2 == 1){
+    console.log("User has unclecked the box");
+  } else {
+    console.log("User has checked the box");
+  }
+
+  clickNo++;
+  console.log("clickNo = " + clickNo);
+  return;
+}
